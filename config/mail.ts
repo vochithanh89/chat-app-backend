4122b36af51ccf0a79980fb5f7bcd075aeda270b
Le Nguyen Quang Vinh <quangvinh7137@gmail.com>
2026-05-23 06:24:34 +0700

    chore: fix SMTP secure config for Render deployment

--- config/mail.ts
+++ config/mail.ts
@@ -13,10 +13,14 @@ const mailConfig = defineConfig({
     smtp: transports.smtp({
       host: env.get('SMTP_HOST'),
       port: env.get('SMTP_PORT'),
+      secure: true,
       auth: {
         type: 'login',
         user: env.get('SMTP_USERNAME'),
         pass: env.get('SMTP_PASSWORD'),
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     }),
 
