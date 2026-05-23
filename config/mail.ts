a9fc37dfb295ea17a0741310368223e1e97c910e
Le Nguyen Quang Vinh <quangvinh7137@gmail.com>
2026-05-23 07:59:51 +0700

    fix smtp config

--- config/mail.ts
+++ config/mail.ts
@@ -3,30 +3,23 @@ import { defineConfig, transports } from '@adonisjs/mail'
 
 const mailConfig = defineConfig({
   default: 'smtp',
-  /**
-   * The mailers object can be used to configure multiple mailers
-   * each using a different transport or same transport with different
-   * options.
-   */
+
   mailers: {
     smtp: transports.smtp({
       host: env.get('SMTP_HOST'),
-      port: env.get('SMTP_PORT'),
+
+      // Ép kiểu sang number
+      port: Number(env.get('SMTP_PORT')),
+
+      // Gmail: 587 => secure false
+      secure: false,
+
       auth: {
         type: 'login',
         user: env.get('SMTP_USERNAME'),
         pass: env.get('SMTP_PASSWORD'),
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
     }),
-
-    // mailgun: transports.mailgun({
-    //     key: env.get('MAILGUN_API_KEY'),
-    //     baseUrl: 'https://api.mailgun.net/v3',
-    //     domain: env.get('MAILGUN_DOMAIN'),
-    // }),
   },
 })
 
